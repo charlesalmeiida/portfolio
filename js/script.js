b6cb4dfba7ff6e547c9e818edbe769b5d46b35d8
charlesalmeiida <charlesalmeida844@gmail.com>
2024-02-29 01:25:12 -0300

    feat

--- js/script.js
+++ js/script.js
@@ -15,3 +15,21 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".btn-prev",
   },
 })
+
+const navMenu = document.querySelectorAll("#js-nav li a")
+
+function scrollToSection(event) {
+  event.preventDefault()
+  const href = event.currentTarget.getAttribute("href")
+  const section = document.querySelector(href)
+  const initPosition = section.offsetTop
+
+  window.scrollTo({
+    top: initPosition,
+    behavior: "smooth",
+  })
+}
+
+navMenu.forEach((link) => {
+  link.addEventListener("click", scrollToSection)
+})
